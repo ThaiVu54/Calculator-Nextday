3df272ba94ae2555c63c3a32e3ec18b55e156164
vukieuanh <vukieuanh.hnue@gmail.com>
2021-09-28 09:59:09 +0700

    refactor test 30/04/2021

--- src/NextDayCalculatorTest.java
+++ src/NextDayCalculatorTest.java
@@ -6,11 +6,14 @@ class NextDayCalculatorTest {
     @Test
     @DisplayName("case 1/1/2021")
     void testDay1Month1Year2021(){
+        //chuẩn bị dữ liệu
         int dayTest =1;
         int monthTest =1;
         int yearTest =2021;
-        String result = NextDayCalculator.getNextDay(dayTest, monthTest, yearTest);
         String expected = "2/1/2021";
+        //thực thi phương thức
+        String result = NextDayCalculator.getNextDay(dayTest, monthTest, yearTest);
+        //so sánh kết quả mong muốn với kết quả chạy đc
         assertEquals(expected, result);
     }
     @Test
@@ -25,7 +28,7 @@ class NextDayCalculatorTest {
     }
     @Test
     @DisplayName("case 30/4/2021")
-    void testDay30Month1Year2021(){
+    void testDay30Month4Year2021(){
         int dayTest =30;
         int monthTest =4;
         int yearTest =2021;
